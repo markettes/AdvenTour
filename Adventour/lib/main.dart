c24bc31688e01d4b550f984816fc3b0432c95962
jotaeme222 <62955243+jotaeme222@users.noreply.github.com>
2020-10-16 17:35:38 +0200

    CenterPosition acabado

--- Adventour/lib/main.dart
+++ Adventour/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:here_sdk/core.dart';
 import 'package:here_sdk/mapview.dart';
 
+
 void main() {
   SdkContext.init(IsolateOrigin.main);
   runApp(MyApp());
@@ -10,12 +11,30 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
+  HereMapController mapController;
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HERE SDK for Flutter - Hello Map!',
-      home: HereMap(onMapCreated: _onMapCreated),
+      home: Scaffold(
+      body: HereMap(onMapCreated: _onMapCreated),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () 
+        {
+         mapController.camera.lookAtPointWithDistance(GeoCoordinates(39.2434, -0.42), 8000);
+        },
+        child: Icon(
+          Icons.my_location,
+          size: 35,
+          color: Colors.black,
+          ),
+        backgroundColor: Colors.deepPurple,
+      ),
+    ),
     );
   }
+
+
+  
 
   void _onMapCreated(HereMapController hereMapController) {
     hereMapController.mapScene.loadSceneForMapScheme(MapScheme.normalDay,
@@ -24,10 +43,10 @@ class MyApp extends StatelessWidget {
         print('Map scene not loaded. MapError: ${error.toString()}');
         return;
       }
-
+      mapController = hereMapController;
       const double distanceToEarthInMeters = 8000;
       hereMapController.camera.lookAtPointWithDistance(
           GeoCoordinates(39.2434, -0.42), distanceToEarthInMeters);
     });
   }
-}
+} 
