5325b0c6c48ca44b2e3f6b882d2512a343ddb6fc
marferp2 <marferp2@inf.upv.es>
2020-10-26 20:04:14 +0100

    Map

--- Adventour/ios/Runner/AppDelegate.swift
+++ Adventour/ios/Runner/AppDelegate.swift
@@ -8,6 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyC7MvBTirNuQarrO0FABDvRyYLxZs9Fhfw")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
