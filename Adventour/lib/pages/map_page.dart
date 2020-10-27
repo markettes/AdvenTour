45573c7e0f9bdddaa4b97394e8aa60656a077662
Zayre <thezayrex@gmail.com>
2020-10-27 20:35:33 +0100

    añadido el botón centrar

--- Adventour/lib/pages/map_page.dart
+++ Adventour/lib/pages/map_page.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart' as Location;
 
 class MapPage extends StatefulWidget {
   @override
@@ -9,30 +12,57 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   GoogleMapController mapController;
+  Timer timer;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
-          future: Geolocator.getCurrentPosition(
-              desiredAccuracy: LocationAccuracy.high),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) print(snapshot.error);
-            if (!snapshot.hasData) return CircularProgressIndicator();
-            Position position = snapshot.data;
-            LatLng currentPosition =
-                LatLng(position.latitude, position.longitude);
-            return GoogleMap(
-              onMapCreated: _onMapCreated,
-              zoomControlsEnabled: false,
-              initialCameraPosition: CameraPosition(
-                target: currentPosition,
-                zoom: 11.0,
-              ),
-              myLocationEnabled: true,
-            );
-          }),
+        future: Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          if (!snapshot.hasData) return CircularProgressIndicator();
+          Position position = snapshot.data;
+          LatLng currentPosition =
+              LatLng(position.latitude, position.longitude);
+          return GoogleMap(
+            onMapCreated: _onMapCreated,
+            zoomControlsEnabled: false,
+            initialCameraPosition: CameraPosition(
+              target: currentPosition,
+              zoom: 11.0,
+            ),
+            myLocationButtonEnabled: false,
+            myLocationEnabled: true,
+          );
+        },
+      ),
+      floatingActionButton:
+            FloatingActionButton.extended(
+              onPressed: _currentLocation,
+              label: Icon(Icons.location_on),
+            ),
     );
+  }
+
+  void _currentLocation() async {
+   final GoogleMapController controller = mapController;
+   Location.LocationData currentLocation;
+   var location = new Location.Location();
+   try {
+     currentLocation = await location.getLocation();
+     } on Exception {
+       currentLocation = null;
+       }
+
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        bearing: 0,
+        target: LatLng(currentLocation.latitude, currentLocation.longitude),
+        zoom: 17.0,
+      ),
+    ));
   }
 
   void _onMapCreated(GoogleMapController controller) {
