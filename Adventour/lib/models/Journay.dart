4d84c28227e54729c4a1a5256a8481a8b9b198ae
daalval <daalval@protonmail.com>
2020-11-01 23:38:02 +0100

    cambiado el ancho del polyline, era muy feo

--- Adventour/lib/models/Journay.dart
+++ Adventour/lib/models/Journay.dart
@@ -1,4 +1,5 @@
 import 'package:Adventour/models/Place.dart';
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_webservice/directions.dart' as directions;
 
@@ -34,13 +35,14 @@ Path toPath(directions.Route route) {
   List<LatLng> points = [];
   for (var leg in route.legs) {
     for (var step in leg.steps) {
-      points.add(LatLng(step.startLocation.lat,step.startLocation.lng));
+      points.add(LatLng(step.startLocation.lat, step.startLocation.lng));
     }
   }
-  print('???????????????????????'+ points.first.latitude.toString());
+  print('???????????????????????' + points.first.latitude.toString());
   Polyline polyline = Polyline(
-    polylineId: PolylineId('polyline'),
-    points: points,
-  );
+      polylineId: PolylineId('polyline'),
+      points: points,
+      color: Colors.blue,
+      width: 2);
   return Path(polyline);
 }
