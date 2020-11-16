e62c70365fdb96667960cf906b31fbfc6f0672b9
diegoroig <diego.roigdrh@gmail.com>
2020-11-16 19:43:46 +0100

    mas lio que en una boda gitana

--- Adventour/lib/pages/history_page.dart
+++ Adventour/lib/pages/history_page.dart
@@ -9,9 +9,18 @@ class HistoryPage extends StatefulWidget {
   _HistoryPageState createState() => _HistoryPageState();
 }
 
+enum SingingCharacter { duration, time }
+
 class _HistoryPageState extends State<HistoryPage> {
-  // DateTime dateTimeStart;
-  // DateTime dateTimeEnd;
+//   Widget _myRadioButton({String title, int value, Function onChanged}) {
+//   return RadioListTile(
+//     value: value,
+//     groupValue: _groupValue,
+//     onChanged: onChanged,
+//     title: Text(title),
+//   );
+// }
+  SingingCharacter _character = SingingCharacter.duration;
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +30,69 @@ class _HistoryPageState extends State<HistoryPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(25),
-        child: Column(
-          children: [
-            Expanded(
-                child: MaterialButton(
-                    color: Colors.deepPurple[600],
-                    onPressed: () async {
-                      final List<DateTime> picked =
-                          await DateRagePicker.showDatePicker(
-                              context: context,
-                              initialFirstDate: new DateTime.now(),
-                              initialLastDate: (new DateTime.now())
-                                  .add(new Duration(days: 7)),
-                              firstDate: new DateTime(2010),
-                              lastDate: new DateTime(2050));
-                      if (picked != null && picked.length == 2) {
-                        print(picked);
-                      }
-                    },
-                    child: new Text(
-                      "Pick date range",
-                      style: TextStyle(color: Colors.white),
-                    ))),
-          ],
+        child: Expanded(
+          child: Column(
+            children: [
+              Expanded(
+                  child: Row(
+                children: [
+                  Expanded(
+                    child: MaterialButton(
+                        color: Theme.of(context).primaryColor,
+                        height: 100,
+                        minWidth: 50,
+                        onPressed: () async {
+                          final List<DateTime> picked =
+                              await DateRagePicker.showDatePicker(
+                                  context: context,
+                                  initialFirstDate: new DateTime.now(),
+                                  initialLastDate: (new DateTime.now())
+                                      .add(new Duration(days: 7)),
+                                  firstDate: new DateTime(2010),
+                                  lastDate: new DateTime(2050));
+                          if (picked != null && picked.length == 2) {
+                            print(picked);
+                          }
+                        },
+                        child: new Text(
+                          "Pick date range",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: const Text('Duration'),
+                          leading: Radio(
+                            value: SingingCharacter.duration,
+                            groupValue: _character,
+                            onChanged: (SingingCharacter value) {
+                              setState(() {
+                                _character = value;
+                              });
+                            },
+                          ),
+                        ),
+                        ListTile(
+                          title: const Text('Time'),
+                          leading: Radio(
+                            value: SingingCharacter.time,
+                            groupValue: _character,
+                            onChanged: (SingingCharacter value) {
+                              setState(() {
+                                _character = value;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
       ),
     );
