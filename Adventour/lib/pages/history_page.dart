c8695eca7c9ae351f2a6ce2764e7e30943d6f535
diegoroig <diego.roigdrh@gmail.com>
2020-11-16 17:43:57 +0100

    Update history_page.dart

--- Adventour/lib/pages/history_page.dart
+++ Adventour/lib/pages/history_page.dart
@@ -1,4 +1,7 @@
+import 'package:Adventour/widgets/circle_icon.dart';
+import 'package:Adventour/widgets/circle_icon_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class HistoryPage extends StatefulWidget {
   @override
@@ -6,8 +9,43 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  DateTime newDateTime;
+
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your route history'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 75,
+                      child: CupertinoDatePicker(
+                        mode: CupertinoDatePickerMode.date,
+                        initialDateTime: DateTime(2000, 1, 1),
+                        onDateTimeChanged: (DateTime newDateTime) {
+                          this.newDateTime = newDateTime;
+                        },
+                      ),
+                    ),
+                  ),
+                  //  Expanded(
+                  //    child: CircleIcon(image: )
+                  //  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
