4bbed5f80145507053a08af0ad7d80c1aee6b5a9
daalval <daalval@inf.upv.es>
2020-11-22 18:24:55 +0100

    hechos los tests de registrar usuario

--- Adventour/test/register_user_test.dart
+++ Adventour/test/register_user_test.dart
@@ -1,10 +1,30 @@
-import 'package:Adventour/controllers/db.dart';
-import 'package:Adventour/models/User.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 void main() {
-  test('registered and storaged user succesfully', () async {
-    User user = new User('prueba111', 'prueba111@gmail.com');
-    await db.addUser(user);
+  test('with email and password', () async {
+    final auth = MockFirebaseAuth();
+    final result = await auth.signInWithEmailAndPassword(
+        email: 'some email', password: 'some password');
+    final user = result.user;
+    expect(user.uid, isNotEmpty);
+    expect(user.displayName, isNotEmpty);
+    expect(auth.authStateChanges(), emitsInOrder([isA<User>()]));
+  });
+
+  test('with google', () async {
+    final auth = MockFirebaseAuth();
+    // Credentials would typically come from GoogleSignIn.
+    final credential = FakeAuthCredential();
+    final result = await auth.signInWithCredential(credential);
+    final user = result.user;
+    expect(user.uid, isNotEmpty);
+    expect(user.displayName, isNotEmpty);
+    expect(auth.authStateChanges(), emitsInOrder([isA<User>()]));
+    expect(user.isAnonymous, isFalse);
   });
 }
+
+class FakeAuthCredential extends Mock implements AuthCredential {}
