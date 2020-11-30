9df07a156a5ca8b8406743060a6631e457906c29
daalval <daalval@inf.upv.es>
2020-11-30 13:41:45 +0100

    nada

--- adventour_admin/lib/controllers/db.dart
+++ adventour_admin/lib/controllers/db.dart
@@ -1,7 +1,5 @@
-import 'package:Adventour/models/User.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:Adventour/models/Route.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class DB {
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
