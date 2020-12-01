2dcf7d407b9c0834f10c75e1926588cbd647c823
cargalau <cargalau@inf.upv.es>
2020-12-01 20:55:11 +0100

    Prueba Delete Stop Test optimizada

--- Adventour/test/delete_stop_test.dart
+++ Adventour/test/delete_stop_test.dart
@@ -9,16 +9,12 @@ import 'package:mockito/mockito.dart';
 import 'dart:math';
 
 Future<void> main() async {
-  var routePage;
-
   test('delete stop', () async {
-    routePage = RoutePage().createState();
     List<String> _placeTypes = [PARK, TOURIST_ATTRACTION, RESTAURANT, MUSEUM];
     Route route = await routeEngine.makeRoute(
         'ChIJb7Dv8ExPYA0ROR1_HwFRo7Q', _placeTypes, MEDIUM_DISTANCE);
-    routePage.route = route;
     expect(route.places.length, 6);
-    routePage.removePlace(route.places[5]);
+    route.removePlace(route.places[5]);
     expect(route.places.length, 5);
   });
 }
