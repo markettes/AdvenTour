4612eff5824ec06c36868179e6104b12db6f1321
diegoroig <diego.roigdrh@gmail.com>
2020-12-11 12:16:04 +0100

    esto no se por que no estaba subido

---
+++ Adventour/test/create_route_test.dart
@@ -0,0 +1,24 @@
+import 'package:Adventour/controllers/directions_engine.dart';
+import 'package:Adventour/controllers/route_engine.dart';
+import 'package:Adventour/models/Place.dart';
+import 'package:Adventour/models/Route.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('create route', () async {
+    List<String> _placeTypes = [PARK, TOURIST_ATTRACTION, RESTAURANT, MUSEUM];
+    Route route = await routeEngine.makeRoute(
+        'ChIJb7Dv8ExPYA0ROR1_HwFRo7Q', _placeTypes, MEDIUM_DISTANCE);
+
+    expect(route.locationId, 'ChIJb7Dv8ExPYA0ROR1_HwFRo7Q');
+    expect(route.locationName, 'Valencia');
+    expect(route.isHighlight, false);
+    expect(route.isRequested, false);
+    expect(route.likes, 0);
+    expect(route.places.length, 6);
+
+    // route.addPlace(Place(0.42144, 0.41123412));
+
+    // expect(route.places.length, 7);
+  });
+}
