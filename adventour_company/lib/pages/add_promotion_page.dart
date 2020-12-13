a1c155b203e436547d344cacf12326a3189701d4
alrauman <alrauman@inf.upv.es>
2020-12-13 19:04:35 +0100

    barra de busqueda

--- adventour_company/lib/pages/add_promotion_page.dart
+++ adventour_company/lib/pages/add_promotion_page.dart
@@ -1,13 +1,20 @@
 
-import 'package:adventour_company/widgets/google_button.dart';
-import 'package:adventour_company/widgets/primary_button.dart';
+import 'package:Adventour/app_localizations.dart';
+import 'package:adventour_company/pages/search_page.dart';
+import 'package:google_maps_webservice/src/places.dart';
 import 'package:flutter/material.dart';
 
+import 'dart:async';
 
 
+// ignore: must_be_immutable
 class AddPromotionPage extends StatelessWidget {
   AddPromotionPage({this.allowSignUp = true});
   bool allowSignUp;
+
+
+   TextEditingController _locationController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +31,42 @@ class AddPromotionPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: Colors.white,
+                      boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 20,
+                          )
+                        ]),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: '    Search', //xd
+                            ),
+                            controller: _locationController,
+                            onTap: () async {
+                              await PlacesAutocomplete.show(
+                                context: context,
+                                onTapPrediction: _onTapPrediction,
+                                onSubmitted: _onSubmitted,
+                              );
+                            },
+                            readOnly: true,
+                            ),
+                        ),
+                        ]
+                      )
+                    )
+                  )
+
                 ],
               ),
             ),
@@ -33,4 +76,13 @@ class AddPromotionPage extends StatelessWidget {
       ),
     );
   }
+
+  Future _onTapPrediction(Prediction prediction) async {
+    
+  }
+
+  Future _onSubmitted(String value) async {
+  }
+
 }
+
