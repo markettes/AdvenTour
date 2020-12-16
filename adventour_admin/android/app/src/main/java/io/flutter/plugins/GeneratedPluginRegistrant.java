6590dd8feacd49aacd91c5b6c9ebea69931b9aa9
daalval <daalval@inf.upv.es>
2020-12-16 18:31:39 +0100

    cambios

--- adventour_admin/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ adventour_admin/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -4,6 +4,7 @@ import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 
 /**
  * Generated file. Do not edit.
@@ -13,12 +14,14 @@ import io.flutter.embedding.engine.FlutterEngine;
 @Keep
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.firestore.FlutterFirebaseFirestorePlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebaseanalytics.FirebaseAnalyticsPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.auth.FlutterFirebaseAuthPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebasedynamiclinks.FirebaseDynamicLinksPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.storage.FlutterFirebaseStoragePlugin());
+      com.sidlatau.flutteremailsender.FlutterEmailSenderPlugin.registerWith(shimPluginRegistry.registrarFor("com.sidlatau.flutteremailsender.FlutterEmailSenderPlugin"));
     flutterEngine.getPlugins().add(new io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin());
     flutterEngine.getPlugins().add(new com.baseflow.geolocator.GeolocatorPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.googlemaps.GoogleMapsPlugin());
--- adventour_company/lib/pages/add_promotion_page.dart
+++ adventour_company/lib/pages/add_promotion_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:Adventour/app_localizations.dart';
 import 'package:Adventour/widgets/input_text.dart';
 import 'package:adventour_company/pages/search_page.dart';
@@ -9,27 +8,22 @@ import 'package:flutter/material.dart';
 
 import 'dart:async';
 
-
 // ignore: must_be_immutable
 class AddPromotionPage extends StatelessWidget {
   AddPromotionPage({this.allowSignUp = true});
   bool allowSignUp;
-  
+
   String name;
   String coords;
   String description;
 
-  TextEditingController _textControllerName =
-      TextEditingController(text: "Name");
+  TextEditingController _textControllerName = TextEditingController();
 
-  TextEditingController _textControllerCoord =
-  TextEditingController(text: "(Lat, Long)");
+  TextEditingController _textControllerCoord = TextEditingController();
 
-  TextEditingController _textControllerDesc =
-  TextEditingController(text: "Description");
+  TextEditingController _textControllerDesc = TextEditingController();
 
-
-   TextEditingController _locationControllerSearch = TextEditingController();
+  TextEditingController _locationControllerSearch = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -41,123 +35,94 @@ class AddPromotionPage extends StatelessWidget {
         padding: EdgeInsets.all(10),
         child: Column(
           children: [
-            Expanded(
-              flex: 3,
-              child: Padding(
-                padding: const EdgeInsets.all(25),
-                child: Image.asset(
-                  'assets/logo_adventour+titulo.png',
-                ),
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: Image.asset(
+                'assets/logo_adventour+titulo.png',
+                height: 175,
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: Container(
-                      decoration: BoxDecoration(
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(40),
                       color: Colors.white,
                       boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 20,
-                          )
-                        ]),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: '    Search your site',
-                            ),
-                            controller: _locationControllerSearch,
-                            onTap: () async {
-                              await PlacesAutocomplete.show(
-                                context: context,
-                                onTapPrediction: _onTapPrediction,
-                                onSubmitted: (value) {},
-                              );
-                            },
-                            readOnly: true,
-                            ),
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 20,
+                        )
+                      ]),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Search...',
+                      icon: Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: Icon(
+                          Icons.search,
                         ),
-                        ]
-                      )
-                    )
-                  )
-
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: [
-                  
-                ]
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: InputText(
-                      controller: _textControllerName,
-                      labelText: 'Name',
-                      )
-                  )
-                ]
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: InputText(
-                      controller: _textControllerCoord,
-                      labelText: 'Coordinates',
-                      )
-                  )
-                ]
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: InputText(
-                      controller: _textControllerDesc,
-                      labelText: 'Description',
-                      )
-                  )
-                ]
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  PrimaryButton(
-                    text: 'Confirm',
-                    onPressed: () async {
-                      
+                      ),
+                    ),
+                    controller: _locationControllerSearch,
+                    onTap: () async {
+                      await PlacesAutocomplete.show(
+                        context: context,
+                        onTapPrediction: _onTapPrediction,
+                        onSubmitted: (value) {},
+                      );
                     },
-                    icon: Icons.add_location_rounded,
-                    style: ButtonType.Normal,
-                  ),
-                ],
+                    readOnly: true,
+                  )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Find your site to fill the fields.',
+              style:
+                  Theme.of(context).textTheme.bodyText2.copyWith(fontSize: 17),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      child: InputText(
+                        controller: _textControllerName,
+                        labelText: 'Name',
+                        readOnly: true,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: InputText(
+                        controller: _textControllerCoord,
+                        labelText: 'Coordinates',
+                        readOnly: true,
+                        icon: Icons.location_on,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    PrimaryButton(
+                      text: 'CONFIRM',
+                      onPressed: () async {},
+                      style: ButtonType.Normal,
+                    )
+                  ],
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -167,7 +132,4 @@ class AddPromotionPage extends StatelessWidget {
   Future _onTapPrediction(Prediction prediction) async {
     String id = prediction.placeId;
   }
-
-  
 }
-
